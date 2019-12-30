7dc92b385524b40d8f1e5b5845b856649793014b
asuendermann <annette@asuendermann.de>
2019-12-30 12:03:17 +0100

    Added Serilog Sink 'File' to HelloCore

--- VolumeOne/HelloCore/Program.cs
+++ VolumeOne/HelloCore/Program.cs
@@ -1,26 +1,38 @@
-﻿using Microsoft.Extensions.Configuration;
-using Serilog;
-using System;
+﻿using System;
 using System.IO;
 
-namespace HelloCore {
-     class Program {
+using Microsoft.Extensions.Configuration;
 
+using Serilog;
+using Serilog.Events;
+
+namespace HelloCore {
+    internal class Program {
         public static string AspnetcoreEnvironment = "ASPNETCORE_ENVIRONMENT";
+
+        public static string AppSettings = "appsettings";
         public static string SerilogSettings = "serilogsettings";
 
-        static void Main(string[] args) {
-            ConfigureSerilogLocally();
-            //ConfigureSerilogFromFile();
-            Log.Information("Hello Core!");
-        }
+        private static void Main(string[] args) {
 
+            Serilog.Debugging.SelfLog.Enable(Console.Error);
+            //ConfigureSerilogLocally();
+
+            ConfigureSerilogFromFile();
+            Log.Verbose("Hello Core! logs Verbose messages");
+            Log.Debug("Hello Core! logs Debug messages");
+            Log.Information("Hello Core! logs Information messages");
+            Log.Warning("Hello Core! logs Warning messages");
+            Log.Error("Hello Core! logs Error messages");
+            Log.Fatal("Hello Core! logs Fatal messages");
+        }
 
         private static void ConfigureSerilogLocally() {
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Verbose()
-                .WriteTo.Console()
+                .WriteTo.Console( restrictedToMinimumLevel: LogEventLevel.Information)
                 .CreateLogger();
+            Log.Information("Serilog Configuration locally succeeded");
         }
 
         private static void ConfigureSerilogFromFile() {
@@ -28,6 +40,7 @@ namespace HelloCore {
             var loggerConfiguration = new LoggerConfiguration()
                 .ReadFrom.Configuration(serilogConfiguration);
             Log.Logger = loggerConfiguration.CreateLogger();
+            Log.Information("Serilog Configuration from file succeeded");
         }
 
         private static IConfigurationRoot ReadSerilogConfiguration() {
@@ -35,6 +48,8 @@ namespace HelloCore {
             var environmentName = Environment.GetEnvironmentVariable(AspnetcoreEnvironment);
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(currentDirectory)
+                .AddJsonFile($"{AppSettings}.json", false, true)
+                .AddJsonFile($"{AppSettings}.{environmentName}.json", true)
                 .AddJsonFile($"{SerilogSettings}.json", false, true)
                 .AddJsonFile($"{SerilogSettings}.{environmentName}.json", true)
                 .AddEnvironmentVariables()
